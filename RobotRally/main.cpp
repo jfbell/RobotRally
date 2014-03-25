84fd2ea4e34c2a42afae177b0ab17916ea9a0473
James Bell <james@James-MBP.local>
2014-03-25 14:12:58 +1100

    Initial Commit

---
+++ RobotRally/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RobotRally
+//
+//  Created by James Bell on 25/03/2014.
+//  Copyright (c) 2014 BellDesign. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
